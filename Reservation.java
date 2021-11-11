743c79551880942b929c502883e645c35936e962
BigSean1 <93724664+BigSean1@users.noreply.github.com>
2021-11-11 18:45:28 +0000

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 /*A Reservation Object*/
@@ -10,12 +11,29 @@ public class Reservation {
 	
 	/*Reservation Constructor*/
 	public Reservation(String name, String type, String roomType, String checkIn, String checkOut) {
-		this.name = name;
-		this.resType = type;
-		this.roomType = roomType;
-		this.checkIn = checkIn;
-		this.checkOut = checkOut;
-		bookings++;
-		refNo = bookings;
+		if(available(roomType, checkIn, checkOut)) {
+			this.name = name;
+			this.resType = type;
+			this.roomType = roomType;
+			this.checkIn = checkIn;
+			this.checkOut = checkOut;
+			bookings++;
+			refNo = bookings;
+		}
+	}
+	
+	public void addRes(Reservation a) {
+		try(FileWriter fw = new FileWriter("Desktop\reservations.csv", true);
+			    BufferedWriter bw = new BufferedWriter(fw);
+			    PrintWriter out = new PrintWriter(bw))
+			{
+			    out.println(name+", "+refNo+", "+resType+", "+roomType+", "+checkIn+", "+checkOut);
+			    		} catch (IOException e) {
+			    
+			}
+	}
+	
+	public boolean available(String roomType, String checkIn, String checkOut) {
+		return false;
 	}
 }
